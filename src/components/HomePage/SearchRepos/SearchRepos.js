661526afb365ee48262eb3ca2919c22872085fb3
nester1van <nester1van@yandex.ru>
2020-06-19 21:37:42 +0400

    fix SearchRepos

--- src/components/HomePage/SearchRepos/SearchRepos.js
+++ src/components/HomePage/SearchRepos/SearchRepos.js
@@ -2,54 +2,34 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux'; 
 import { getRepos, setQuery, setCurrentPage } from '../../../redux/reposWithCommits/actions';
 
-let componentDidMount = false;
-const SearchRepos = ({totalCount, currentPage, perPage, query, getRepos, setQuery, setCurrentPage}) => {  
 
+const SearchRepos = ({totalCount, currentPage, perPage, query, getRepos, setQuery, setCurrentPage}) => {  
+  
   const handleChange = (e) => {
     const value = e.target.value;
     setQuery(value);
   };
 
   useEffect(() => {
-    //localStorage.setItem('test', 1);
-    //console.log(localStorage.getItem('test'));
-    // currentPage query
-
-    //componentDidMount = true;
-  }, []);
-
-  useEffect(() => {
-    // max = Math.ceil(totalCount / perPage);
-    // if (currentPage > max) {
-    //   setCurrentPage(max);
-    //   currentPage = max;
-    // }
-    let timerId;
-    if (componentDidMount) {
-      timerId = setTimeout(() => {
+      let   timerId = setTimeout(() => {
         if (localStorage.getItem('query') !== query) {
           localStorage.setItem('query', query);
           localStorage.setItem('currentPage', 1);
           setCurrentPage(1);
           getRepos(query, perPage, 1);
-        } else {
-          //getRepos(query, perPage, currentPage);
         }
       }, 1000);
-    }
     return () => {
-      componentDidMount = true;
       clearTimeout(timerId);}
   }, [query]);
 
-
   return (
     <>
-        <input 
-          onChange={handleChange}
-          type="search" 
-          placeholder="search.." 
-          value={query}/>
+      <input 
+        onChange={handleChange}
+        type="search" 
+        placeholder="search.." 
+        value={query}/>
     </>
   )
 };
